e1e46ea13a51642b9c069bfe590c9b2fb80f082f
Matthew Hicks <mkigikm@gmail.com>
2015-03-10 13:42:40 -0400

    adds signup form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,13 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  helper_method :auth_token
+
+  def auth_token
+    <<-HTML.html_safe
+    <input type="hidden" name="authenticity_token"
+      value="#{form_authenticity_token}">
+    HTML
+  end
 end
