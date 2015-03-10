d284e1115543f5031ff53e86e3f6b59f2a8ce144
Matthew Hicks <mkigikm@gmail.com>
2015-03-10 14:08:59 -0400

    complete signup form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,12 +3,20 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  helper_method :auth_token
+  helper_method :auth_token, :error_messages
 
   def auth_token
     <<-HTML.html_safe
     <input type="hidden" name="authenticity_token"
       value="#{form_authenticity_token}">
     HTML
+  end
+
+  def error_messages(model, key)
+    if model.errors.keys.include?(key)
+      <<-HTML.html_safe
+      <strong>#{model.errors.full_messages_for(key).join(" ")}</strong>
+      HTML
+    end
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,5 +5,17 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(user_params)
+
+    if @user.save
+      render text: "profile"
+    else
+      render :new
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:email, :password)
   end
 end
