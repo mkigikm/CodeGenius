1be047d0842d1388b5b5ba09986290bc27ff985a
Matthew Hicks <mkigikm@gmail.com>
2015-03-10 12:26:07 -0400

    adds uniqueness validation to session_token

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   validates :email, :session_token, :password_digest, presence: true
-  validates :email, uniqueness: true
+  validates :email, :session_token, uniqueness: true
   validates :password, length: { minimum: 6, allow_nil: true }
 
   attr_reader :password
