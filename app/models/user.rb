2e1740e940e19cf259b4d971109e19b920087b03
Matthew Hicks <mkigikm@gmail.com>
2015-03-10 11:51:57 -0400

    makes user model pass account creation specs

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,29 @@ class User < ActiveRecord::Base
   validates :password, length: { minimum: 6, allow_nil: true }
 
   attr_reader :password
+
+  after_initialize :ensure_session_token
+
+  def password=(new_password)
+    @password = new_password
+    self.password_digest = BCrypt::Password.create(@password)
+  end
+
+  private
+  def self.used_token?(session_token)
+    !!User.find_by(session_token: session_token)
+  end
+
+  def generate_session_token
+    new_token = nil
+    while !new_token || self.class.used_token?(new_token)
+      new_token = SecureRandom.urlsafe_base64
+    end
+
+    new_token
+  end
+
+  def ensure_session_token
+    self.session_token ||= generate_session_token
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,20 +1,18 @@
-require 'rails_helper'
-
 describe User, type: :model do
   describe "account creation" do
     it "requires an email address" do
       u = User.create(password: "password")
-      expect(u.valid?).to_be false
+      expect(u.valid?).to be false
     end
 
     it "requires a password of at least length 6" do
       u = User.create(email: "matt@example.com", password: "passw")
-      expect(u.valid?).to_be false
+      expect(u.valid?).to be false
     end
 
     it "creates a session token" do
       u = User.create(email: "matt@example.com", password: "password")
-      expect(u.session_token).to exist
+      expect(u.session_token).to_not be_nil
     end
 
     it "doesn't save the password to the database" do
