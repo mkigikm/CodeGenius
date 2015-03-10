0c638cf04b033707f044483963ed6ce8d08e2fe1
Matthew Hicks <mkigikm@gmail.com>
2015-03-10 12:02:35 -0400

    refactors unique email test, reformats

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,29 +3,35 @@ describe User, type: :model do
     it "requires an email address" do
       u = User.new(password: "password")
       u.valid?
+
       expect(u.errors.keys).to include :email
     end
 
     it "requires a password of at least length 6" do
       u = User.new(email: "matt@example.com", password: "passw")
       u.valid?
+
       expect(u.errors.keys).to include :password
     end
 
     it "creates a session token" do
       u = User.new(email: "matt@example.com", password: "password")
+
       expect(u.session_token).to_not be_nil
     end
 
     it "doesn't save the password to the database" do
       u = User.create(email: "matt@example.com", password: "password")
       u = User.find_by(email: "matt@example.com")
+
       expect(u.password).to be_nil
     end
 
     it "requires unique email addresses" do
       u0 = User.create(email: "matt@example.com", password: "password")
-      u1 = User.create(email: "matt@example.com", password: "password")
+      u1 = User.new(email: "matt@example.com", password: "password")
+      u1.valid?
+      
       expect(u1.errors.keys).to include :email
     end
   end
