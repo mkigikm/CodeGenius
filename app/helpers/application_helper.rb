3cea1fcc8e5f6ed7c439b358958afa51f8400e33
Matthew Hicks <mkigikm@gmail.com>
2015-03-10 22:07:53 -0400

    gets rid of uneccessary line in annotate_phile

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,22 +1,17 @@
 module ApplicationHelper
   def annotate_phile(phile)
-    offset = 0
-    annotated_body = ""
-
-    return phile.body if phile.annotations.empty?
+    offset, annotated_body = 0, ""
 
     phile.annotations.each do |annotation|
       start, finish = annotation.start, annotation.finish
 
-      annotated_body += h(phile.body[offset..(start - 1)]) if start - offset > 0
+      annotated_body += h(phile.body[offset..(start - 1)]) if start > 0
       annotated_body += "<a href='#{annotation_url(annotation)}'>" +
         h(phile.body[start..finish]) + "</a>"
 
       offset = finish + 1
     end
 
-    remainder = phile.annotations.last.finish + 1
-
-    (annotated_body + h(phile.body[remainder..-1])).html_safe
+    (annotated_body + h(phile.body[offset..-1])).html_safe
   end
 end
