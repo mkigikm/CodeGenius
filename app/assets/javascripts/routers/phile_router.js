a454c394427a0a80b8f711c2e12e7411d5535017
Matthew Hicks <mkigikm@gmail.com>
2015-03-11 16:45:22 -0400

    remove uncalled functions from phile_router

--- app/assets/javascripts/routers/phile_router.js
+++ app/assets/javascripts/routers/phile_router.js
@@ -65,29 +65,5 @@ CodeGenius.Routers.PhileRouter = Backbone.Router.extend({
     this._noteView && this._noteView.remove();
     this._noteView = newView;
     $(".file-annotation").html(newView.render().$el);
-  },
-
-  findTextNodeOffset: function (node, nodeList) {
-    var offset = 0;
-
-    _.find(this.findTextNodes(), function (curNode) {
-      offset += curNode.length;
-      return curNode === node;
-    });
-
-    return offset - node.length;
-  },
-
-  findTextNodes: function ($el, nodeList) {
-    $el      || ($el = $(this.$el.find("pre")));
-    nodeList || (nodeList = []);
-
-    _.each($el.contents(), function (node) {
-      //text nodes have a nodeType === 3
-      node.nodeType === 3 ? nodeList.push(node) :
-          this.findTextNodes($(node), nodeList);
-    }, this);
-
-    return nodeList;
   }
-})
+});
