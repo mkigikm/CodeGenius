95f42d2549816585f9191e34f85a9281b69368fd
Matthew Hicks <mkigikm@gmail.com>
2015-03-10 21:20:49 -0400

    add links to annotations

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,22 @@
 module ApplicationHelper
+  def annotate_phile(phile)
+    offset = 0
+    annotated_body = ""
+
+    return phile.body if phile.annotations.empty?
+
+    phile.annotations.each do |annotation|
+      start, finish = annotation.start, annotation.finish
+
+      annotated_body += h(phile.body[offset..(start - 1)]) if start - offset > 0
+      annotated_body += "<a href='#{annotation_url(annotation)}'>" +
+        h(phile.body[start..finish]) + "</a>"
+
+      offset = finish + 1
+    end
+
+    remainder = phile.annotations.last.finish + 1
+
+    (annotated_body + h(phile.body[remainder..-1])).html_safe
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   resources :users, only: [:new, :create, :show]
   resource :session, only: [:new, :create, :destroy]
   resources :philes, only: [:create, :show]
+  resources :annotations, only: [:show]
 end
