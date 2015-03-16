c876aae21c5d6365c762fc8b32e846480253703c
Matthew Hicks <mkigikm@gmail.com>
2015-03-16 09:43:39 -0400

    change per page to 20 so it fits on my screen without a scrollbar :P

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -3,7 +3,7 @@ class SearchesController < ApplicationController
     PgSearch.multisearch_options = {using: {:tsearch => {:prefix => true}}}
     @search_results = PgSearch
       .multisearch(params[:query])
-      .page(params[:page])
+      .page(params[:page]).per(20)
     @no_search = true
   end
 end
