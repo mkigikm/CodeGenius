eeba598ece43b51c8faf6d97aab21391cd92f01d
Matthew Hicks <mkigikm@gmail.com>
2015-03-18 11:38:22 -0400

    orders notifications correctly

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -3,4 +3,6 @@ class Notification < ActiveRecord::Base
   belongs_to :notifiable, inverse_of: :notifications, polymorphic: true
 
   validates :notifiable, :user, presence: true
+
+  default_scope { order("created_at DESC") }
 end
